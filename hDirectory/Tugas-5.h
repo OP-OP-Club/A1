22cda69679e296295f44411f28bf03404c24e745
Matthew1906 <matthew.mulyono001@binus.ac.id>
2020-11-13 13:32:07 +0700

    Sorry gk teliti

--- hDirectory/Tugas-5.h
+++ hDirectory/Tugas-5.h
@@ -21,16 +21,21 @@ void orderHistory() {
 		getchar();
 		return;
 	}
-    puts("| No  | Name                 | Price  | Topping    | Callories  | Flavor     | size  | Order Time              |");
-    puts("----------------------------------------------------------------------------------------------------------------");
+    puts("| No  | Name                 | Price  | Topping    | Callories  | Flavor     | size  | Order Time           |");
+    puts(" -----------------------------------------------------------------------------------------------------------");
     int index = 1;
     for(int i = 0; i<DessertLen; i++){
-        if(ArrayDessert[i].dessert_cooking_time<=0)("| %-4d| %-21s| %-7d| %-11s| %-11.2f| -          | -     | %s  |\n", index, ArrayDessert[i].food_name, ArrayDessert[i].food_price, ArrayDessert[i].dessert_topping, ArrayDessert[i].dessert_calories, ArrayDessert[i].created_at);
-        index++;
+        if(ArrayDessert[i].dessert_cooking_time<=0){
+			printf("| %-4d| %-21s| %-7d| %-11s| %-11.2f| -          | -     | %s  |\n", index, ArrayDessert[i].food_name, ArrayDessert[i].food_price, ArrayDessert[i].dessert_topping, ArrayDessert[i].dessert_calories, ArrayDessert[i].created_at);
+        	index++;
+		}
     }
     for(int i = 0; i<DrinkLen; i++){
-        if(ArrayDrink[i].drink_cooking_time<=0)("| %-4d| %-21s| %-7d| -          |-           | %-11s| %c     | %s  |\n", index, ArrayDrink[i].food_name, ArrayDrink[i].food_price, ArrayDrink[i].drink_flavor, ArrayDrink[i].drink_size, ArrayDrink[i].created_at);
-    }
+        if(ArrayDrink[i].drink_cooking_time<=0){
+			printf("| %-4d| %-21s| %-7d| -          |-           | %-11s| %c     | %s  |\n", index, ArrayDrink[i].food_name, ArrayDrink[i].food_price, ArrayDrink[i].drink_flavor, ArrayDrink[i].drink_size, ArrayDrink[i].created_at);
+			index++;
+		}
+	}
     puts("");
     printf("Press Enter to continue");
     getchar();
