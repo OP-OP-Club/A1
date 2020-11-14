5df4fcf98c8f7ef782a278896bdad392fbfe8c15
NaCl586 <gerson.natanael@binus.ac.id>
2020-11-14 17:05:38 +0700

    Pengecekan apakah ada cooking process

--- hDirectory/Tugas-4.h
+++ hDirectory/Tugas-4.h
@@ -2,8 +2,44 @@
 #include "global.h"
 
 void showCookingProcess(){
-        int a=1;
-        int b=2;
-        printf("%d\n", a+b);
-        printf("%d\n", a%b);
+        ClearScreen();
+        int flag = 0; //tanda apakah ada yang masih dimasak ato nggak (0 = tidak)
+
+        //cek apakah struct dessert/drink ada isinya ato nggak, kalo ga ada berarti ga ada yang dimasak --> flag = 0
+        if(DessertLen != 0 || DrinkLen != 0){
+            flag = 1;
+        }
+
+        //kalau ada yg udah ada isi, apakah lagi dimasak ato nggak (cooking time > 0)
+        if(flag == 1){
+            //bikin flag jadi 0 lagi karena bisa aja semua isinya cooking time == 0
+            flag = 0;
+            //cek dessert
+            if(DessertLen != 0){
+                for(int i=0;i<DessertLen;i++){
+                    if(ArrayDessert[i].dessert_cooking_time > 0){
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+            //hanya cek drink apabila flag masih 0, krn kan kalau udh ada yg in progress di dessert, pasti flag tetep akan 1
+            if(flag == 0 && DrinkLen != 0){
+                for(int i=0;i<DrinkLen;i++){
+                    if(ArrayDrink[i].drink_cooking_time > 0){
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+            
+        }
+        //kalau misal flag==0 berarti ga ada yg lagi cooking progress/ga ada isi struct array nya
+        if(flag == 0){
+            printf("There is no Dessert or Drink on the list!\n\n");
+            printf("Press Enter to continue");
+            getchar();
+        }else{
+            //ini kalau misalkan ada cooking process
+        }
 }
