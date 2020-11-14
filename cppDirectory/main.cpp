dadc94f0bc01fd8c46fccf784b3523c4e52fa645
NaCl586 <gerson.natanael@binus.ac.id>
2020-11-14 15:37:12 +0700

    Init files di Tugas-4

--- cppDirectory/main.cpp
+++ cppDirectory/main.cpp
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include "../hDirectory/global.h"
 #include "../hDirectory/Tugas-5.h"
+#include "../hDirectory/Tugas-4.h"
 
 int main(){
 
 	orderHistory();
-
+    showCookingProcess();
     return 0;
 }
---
+++ hDirectory/Tugas-4.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include "global.h"
+
+//tugas 4 testing
+
+void showCookingProcess(){
+    printf("Test\n");
+}
