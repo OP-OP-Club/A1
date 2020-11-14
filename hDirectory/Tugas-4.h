db56355781f46bd2f3ad1dbb45390743a5b816a8
NaCl586 <gerson.natanael@binus.ac.id>
2020-11-14 18:04:58 +0700

    validasi kalau pas buka bikin cooking time = 0

--- hDirectory/Tugas-4.h
+++ hDirectory/Tugas-4.h
@@ -4,54 +4,7 @@
 
 void showCookingProcess(){
 
-        //buat debug nambahin 4 makanan:
-        strcpy(ArrayDessert[0].food_name, "Lime Ball");
-        ArrayDessert[0].food_price = 20;
-        strcpy(ArrayDessert[0].dessert_topping,  "Honey");
-        ArrayDessert[0].dessert_calories = 15.50;
-        ArrayDessert[0].dessert_cooking_time = 62;
-
-        strcpy(ArrayDessert[1].food_name, "ChocoLava");
-        ArrayDessert[1].food_price = 15;
-        strcpy(ArrayDessert[1].dessert_topping,  "Caramel");
-        ArrayDessert[1].dessert_calories = 2.50;
-        ArrayDessert[1].dessert_cooking_time = 64;
-
-        DessertLen = 2;
-
-        strcpy(ArrayDrink[0].food_name, "Mojito");
-        ArrayDrink[0].food_price = 15;
-        strcpy(ArrayDrink[0].drink_flavor, "Mint");
-        ArrayDrink[0].drink_size = 'M';
-        ArrayDrink[0].drink_cooking_time = 10;
-
-        strcpy(ArrayDrink[1].food_name, "Coca Cola");
-        ArrayDrink[1].food_price = 10;
-        strcpy(ArrayDrink[1].drink_flavor, "Mix Berry");
-        ArrayDrink[1].drink_size = 'S';
-        ArrayDrink[1].drink_cooking_time = 20;
-
-        DrinkLen = 2;
-
         ClearScreen();
-
-        for(int k=0;k<DrinkLen;k++)
-        {
-            ArrayDrink[k].drink_cooking_time-=10;
-            if(ArrayDrink[k].drink_cooking_time<=0)
-            {
-                ArrayDrink[k].drink_cooking_time=0;
-            }
-        }
-
-        for(int k=0;k<DessertLen;k++)
-        {
-            ArrayDessert[k].dessert_cooking_time-=10;
-            if(ArrayDessert[k].dessert_cooking_time<=0)
-            {
-                ArrayDessert[k].dessert_cooking_time=0;
-            }
-        }
 
         int flag = 0; //tanda apakah ada yang masih dimasak ato nggak (0 = tidak)
         int numberProcess=1;
@@ -64,26 +17,32 @@ void showCookingProcess(){
         if(flag == 1){
             //bikin flag jadi 0 lagi karena bisa aja semua isinya cooking time == 0
             flag = 0;
-            //cek dessert
+            //cek dessert dan kurangi 10 detik
             if(DessertLen != 0){
                 for(int i=0;i<DessertLen;i++){
+                    ArrayDessert[i].dessert_cooking_time-=10;
+                    if(ArrayDessert[i].dessert_cooking_time<=0) {
+                        ArrayDessert[i].dessert_cooking_time=0;
+                    }
                     if(ArrayDessert[i].dessert_cooking_time > 0){
                         flag = 1;
-                        break;
                     }
                 }
             }
-            //hanya cek drink apabila flag masih 0, krn kan kalau udh ada yg in progress di dessert, pasti flag tetep akan 1
-            if(flag == 0 && DrinkLen != 0){
+            //cek drink dan kurangi 10 detik
+            if(DrinkLen != 0){
                 for(int i=0;i<DrinkLen;i++){
+                    ArrayDrink[i].drink_cooking_time-=10;
+                    if(ArrayDrink[i].drink_cooking_time<=0){
+                        ArrayDrink[i].drink_cooking_time=0;
+                    }
                     if(ArrayDrink[i].drink_cooking_time > 0){
                         flag = 1;
-                        break;
                     }
                 }
-            }
-            
+            }   
         }
+
         //kalau misal flag==0 berarti ga ada yg lagi cooking progress/ga ada isi struct array nya
         if(flag == 0){
             printf("There is no Dessert or Drink on the list!\n\n");
@@ -95,16 +54,18 @@ void showCookingProcess(){
             puts("----------------------------------------------------------------------");
             for(int j=0;j<DessertLen;j++)
             {
-                printf("| %-3d | Dessert       | %-24s| $%-3d   | %-2d      s |\n", numberProcess, ArrayDessert[j].food_name , ArrayDessert[j].food_price, ArrayDessert[j].dessert_cooking_time);
-                numberProcess++;
-                ArrayDessert[j].dessert_cooking_time-=10;
+                if(ArrayDessert[j].dessert_cooking_time > 0){
+                    printf("| %-3d | Dessert       | %-24s| $%-3d   | %-2d      s |\n", numberProcess, ArrayDessert[j].food_name , ArrayDessert[j].food_price, ArrayDessert[j].dessert_cooking_time);
+                    numberProcess++;
+                }
             }
 
             for(int j=0;j<DrinkLen;j++)
             {
-                printf("| %-3d | Drink         | %-24s| $%-3d   | %-2d      s |\n", numberProcess, ArrayDrink[j].food_name , ArrayDrink[j].food_price, ArrayDrink[j].drink_cooking_time);
-                numberProcess++;
-                ArrayDrink[j].drink_cooking_time-=10;
+                if(ArrayDrink[j].drink_cooking_time > 0){
+                    printf("| %-3d | Drink         | %-24s| $%-3d   | %-2d      s |\n", numberProcess, ArrayDrink[j].food_name , ArrayDrink[j].food_price, ArrayDrink[j].drink_cooking_time);
+                    numberProcess++;
+                }
             }
             printf("\n");
             printf("Press Enter to return to main menu\n");
