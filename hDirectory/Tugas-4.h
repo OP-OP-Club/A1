5911ebab8b693220a34e031839f1000d93b80023
NaCl586 <gerson.natanael@binus.ac.id>
2020-11-14 16:48:08 +0700

    testing merge lagi

--- hDirectory/Tugas-4.h
+++ hDirectory/Tugas-4.h
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include "global.h"
 
-//tugas 4 testing
-//tugas 4 testing lagi
-//tambah lagi ya
 
 void showCookingProcess(){
-    printf("Test\n");
-    printf("test lagi 2\n");
-    printf("test lagi 3\n");
-    printf("testing conflict\n");
+    int testing = 0;
 }
