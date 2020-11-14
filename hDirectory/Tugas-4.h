a9bb116fad84a09bc164212ad1b45d86699b51f0
NaCl586 <gerson.natanael@binus.ac.id>
2020-11-14 18:28:43 +0700

    Validasi Profit cm nambah pas time 0 pertama kali

--- hDirectory/Tugas-4.h
+++ hDirectory/Tugas-4.h
@@ -13,6 +13,19 @@ void showCookingProcess(){
             flag = 1;
         }
 
+        int DessertFlag[500] = {};
+        int DrinkFlag[500] = {};
+        for(int i=0;i<DessertLen;i++){
+            if(ArrayDessert[i].dessert_cooking_time == 0){
+                DessertFlag[i] = 1;
+             }
+        }
+        for(int i=0;i<DrinkLen;i++){
+            if(ArrayDrink[i].drink_cooking_time == 0){
+                DrinkFlag[i] = 1;
+             }
+        }
+
         //kalau ada yg udah ada isi, apakah lagi dimasak ato nggak (cooking time > 0)
         if(flag == 1){
             //bikin flag jadi 0 lagi karena bisa aja semua isinya cooking time == 0
@@ -23,7 +36,9 @@ void showCookingProcess(){
                     ArrayDessert[i].dessert_cooking_time-=10;
                     if(ArrayDessert[i].dessert_cooking_time<=0) {
                         ArrayDessert[i].dessert_cooking_time=0;
-                        PROFIT += ArrayDessert[i].food_price;
+                        if(DessertFlag[i] == 0){
+                            PROFIT += ArrayDessert[i].food_price;
+                        }
                     }
                     if(ArrayDessert[i].dessert_cooking_time > 0){
                         flag = 1;
@@ -36,7 +51,9 @@ void showCookingProcess(){
                     ArrayDrink[i].drink_cooking_time-=10;
                     if(ArrayDrink[i].drink_cooking_time<=0){
                         ArrayDrink[i].drink_cooking_time=0;
-                        PROFIT += ArrayDrink[i].food_price;
+                        if(DrinkFlag[i] == 0){
+                            PROFIT += ArrayDrink[i].food_price;
+                        }
                     }
                     if(ArrayDrink[i].drink_cooking_time > 0){
                         flag = 1;
@@ -47,10 +64,12 @@ void showCookingProcess(){
 
         //kalau misal flag==0 berarti ga ada yg lagi cooking progress/ga ada isi struct array nya
         if(flag == 0){
+            //printf("Profit: $%d\n\n", PROFIT);
             printf("There is no Dessert or Drink on the list!\n\n");
             printf("Press Enter to continue");
             getchar();
         }else{
+            //printf("Profit: $%d\n\n", PROFIT);
             //ini kalau misalkan ada cooking process
             puts("| No  | Type          | Name                    | Price  | Time Left |");
             puts("----------------------------------------------------------------------");
